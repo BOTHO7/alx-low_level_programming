31b3046bb64bdeb43fd8089935a2699dd37dcc54
BOTHO7 <bothot.khumalo@gmail>
2022-09-13 02:33:46 -0700

    3. alphABET

---
+++ 3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Printsalphabet in lower and upper case
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar (c);
+
+	putchar('\n');
+
+	return (0);
+}
